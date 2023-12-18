9ca1c8289249886f32887ba0f934badd783f8442
EC2 Default User <ec2-user@ip-172-31-5-108.eu-west-1.compute.internal>
2023-12-18 12:35:22 +0000

    changing the frontend

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,49 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('index', { title: 'McRedeem', name:null });
 });
 
 router.post('/', function(req, res, next) {
 
-  res.render('index', { title: 'Welcome to Web3 BootStrap App', name:req.body.name });
+  res.render('index', { title: 'McRedeem', name:req.body.name });
 });
 
 router.get('/page1', function(req, res, next) {
-  res.render('page1', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page1', { title: '', name:null });
 });
 
 router.get('/page2', function(req, res, next) {
-  res.render('page2', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page2', { title: 'McRedeem', name:null });
 });
 
 router.get('/page3', function(req, res, next) {
-  res.render('page3', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page3', { title: 'McRedeem', name:null });
 });
 
 router.get('/page4', function(req, res, next) {
-  res.render('page4', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page4', { title: 'McRedeem', name:null });
 });
 
 
 router.get('/page5', function(req, res, next) {
-  res.render('page5', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page5', { title: 'What you can get', name:null });
 });
 
 router.get('/page6', function(req, res, next) {
-  res.render('page6', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page6', { title: 'Verifying', name:null });
 });
-
 
 router.get('/page7', function(req, res, next) {
-  res.render('page7', { title: 'Welcome to Web3 BootStrap App', name:null });
+  res.render('page7', { title: 'Thank you!', name:null });
 });
 
-
+router.get('/voting', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/voting.html'),{ contentType: 'text/html' });
+});
 
 module.exports = router;
