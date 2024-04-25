e9fcc9b15f975075b038992896ee20cda5c72f5c
Ankitha Nelaturi <nelaturiankitha@gmail.com>
2024-04-25 20:15:26 +0100

    removing cors from app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,10 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: "*",
-  credentials : true
-}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
