4c3a830c551d13d4a1e5801aae65acf9ad32aa8e
Ankitha Nelaturi <nelaturiankitha@gmail.com>
2024-04-29 03:04:40 +0100

    adding API

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const cors = require('cors');
+const axios = require('axios');
+
 
 var app = express();
 
@@ -12,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,6 +35,21 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  app.post('/', async (req, res) => {
+    const name = req.body.name;
+  
+    try {
+      // Fetch a random quote from the Zen Quotes API
+      const response = await axios.get('https://zenquotes.io/api/random');
+      const quote = response.data[0].q; // Assuming the quote is stored in the 'q' property
+  
+      res.render('index', { name, quote });
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+      res.render('index', { name });
+    }
+  });
 
   // render the error page
   res.status(err.status || 500);
